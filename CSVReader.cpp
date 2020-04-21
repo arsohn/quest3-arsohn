4ca86174b5eae4d1b5017143e291cb9025b45778
arsohn <60234960+arsohn@users.noreply.github.com>
2020-04-20 21:51:31 -0500

    Version 2
    Added results for overload operators of boolean and artimetic

--- CSVReader.cpp
+++ CSVReader.cpp
@@ -5,7 +5,53 @@ CSVReader::CSVReader(string n) {
 }
 
 void CSVReader::compute() {
+	RationalFraction r1;
+	RationalFraction r2;
+	string line;
+	while (getline(ifs, line)) { // getline gets line from input file, then stores in string variable
+		char* str = (char*)line.c_str();  // converts string to char*
+		char* nextstr;
 
+		// will include conversions
+		// get num and den of r1 and r2 as well as operation
+		// operation string will  = an operation
+		// result = result from operator overload
+
+		if (operation == "+") {
+			RationalFraction(r1 + r2);
+			cout << result.getNum() << "/" << result.getDen() << endl;
+		}
+		else if (operation == "-") {
+			RationalFraction(r1 - r2);
+			cout << result.getNum() << "/" << result.getDen() << endl;
+		}
+		else if (operation == "*") {
+			RationalFraction(r1 * r2);
+			cout << result.getNum() << "/" << result.getDen() << endl;
+		}
+		else if (operation == "/") {
+			RationalFraction(r1 / r2);
+			cout << result.getNum() << "/" << result.getDen() << endl;
+		}
+		else if (operation == "==") {
+			cout << (r1 == r2) << endl;
+		}
+		else if (operation == "!=") {
+			cout << (r1 != r2) << endl;
+		}
+		else if (operation == ">") {
+			cout << (r1 > r2) << endl;
+		}
+		else if (operation == "<") {
+			cout << (r1 < r2) << endl;
+		}
+		else if (operation == ">=") {
+			cout << (r1 >= r2) << endl;
+		}
+		else if (operation == "<=") {
+			cout << (r1 <= r2) << endl;
+		}
+	}
 }
 
 CSVReader::~CSVReader() {
