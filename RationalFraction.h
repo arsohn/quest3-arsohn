e6fbab46caa3cf3d6acb5cfd211a4877183ba95a
arsohn <60234960+arsohn@users.noreply.github.com>
2020-04-21 20:46:20 -0500

    Final RationalFraction.h
    Final

--- RationalFraction.h
+++ RationalFraction.h
@@ -1,33 +1,29 @@
-#pragma once
 #ifndef RATIONAL_FRACTION_H
 #define RATIONAL_FRACTION_H
 
-using namespace std;
-
-int getGCD(int n1, int n2); // greatest common divisor
+int getGCD(int n1, int n2);
 
 class RationalFraction {
+	int num;
+	int den;
 public:
 	RationalFraction(int n1 = 0, int n2 = 0);
 	RationalFraction(const RationalFraction& r);
-	void reduceFration();
-	int getNum()const; // getter for numerator
-	int getDen()const; // getter for denominator
-	void setNum(int n); // setter for numerator
-	void setDen(int n); // setter for denominator
-	RationalFraction operator+(const RationalFraction& r); // overload operators for artimetic +
-	RationalFraction operator-(const RationalFraction& r); // overload operators for artimetic -
-	RationalFraction operator*(const RationalFraction& r); // overload operators for artimetic *
-	RationalFraction operator/(const RationalFraction& r); // overload operators for artimetic /
-	bool operator==(const RationalFraction& r);	// overload operators for boolean ==
-	bool operator!=(const RationalFraction& r); // overload operators for boolean !=
-	bool operator>(const RationalFraction& r);  // overload operators for boolean >
-	bool operator<(const RationalFraction& r);  // overload operators for boolean <
-	bool operator>=(const RationalFraction& r); // overload operators for boolean >=
-	bool operator<=(const RationalFraction& r); // overload operators for boolean <=
-private:
-	int num; //numerator
-	int den; //denomiator 
+	void reduceFraction();
+	int getNum()const;
+	int getDen()const;
+	void setNum(int n);
+	void setDen(int n);
+	RationalFraction operator+(const RationalFraction& r);
+	RationalFraction operator-(const RationalFraction& r);
+	RationalFraction operator*(const RationalFraction& r);
+	RationalFraction operator/(const RationalFraction& r);
+	bool operator==(const RationalFraction& r);
+	bool operator!=(const RationalFraction& r);
+	bool operator>(const RationalFraction& r);
+	bool operator<(const RationalFraction& r);
+	bool operator>=(const RationalFraction& r);
+	bool operator<=(const RationalFraction& r);
 };
 
-#endif // !RATIONAL_FRACTION_H
+#endif
