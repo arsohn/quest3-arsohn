2b57766e231d1b90123e7153b9d7c110d8c14e7a
arsohn <60234960+arsohn@users.noreply.github.com>
2020-04-21 20:48:30 -0500

    FINAL RATIONALFRACTION.CPP

--- RationalFraction.cpp
+++ RationalFraction.cpp
@@ -9,7 +9,7 @@ int getGCD(int n1, int n2) {
 		n2 = -n2;
 	}
 	for (int i = 1; i <= n1 && i <= n2; ++i) {
-		if ((n1 % i == 0) && (n2 % i == 0)) {
+		if (n1 % i == 0 && n2 % i == 0) {
 			gcd = i;
 		}
 	}
@@ -20,86 +20,71 @@ RationalFraction::RationalFraction(int n1, int n2) {
 	num = n1;
 	den = n2;
 }
-
 RationalFraction::RationalFraction(const RationalFraction& r) {
 	this->num = r.num;
 	this->den = r.den;
 }
-
-void RationalFraction::reduceFration() {
+void RationalFraction::reduceFraction()
+{
 	int d = getGCD(num, den);
 	num /= d;
-	num /= d;
+	den /= d;
 }
-
 int RationalFraction::getNum()const {
 	return num;
 }
-
 int RationalFraction::getDen()const {
 	return den;
 }
-
 void RationalFraction::setNum(int n) {
 	num = n;
 }
-
 void RationalFraction::setDen(int n) {
 	den = n;
 }
-
-RationalFraction RationalFraction::operator+(const RationalFraction& r) { // + operator overload
+RationalFraction RationalFraction::operator+(const RationalFraction& r) {
 	RationalFraction result;
 	result.den = this->den * r.den;
 	result.num = ((result.den / this->den) * this->num) + ((result.den / r.den) * r.num);
-	result.reduceFration();
+	result.reduceFraction();
 	return result;
 }
-
-RationalFraction RationalFraction::operator-(const RationalFraction& r) { // - operator overload
+RationalFraction RationalFraction::operator-(const RationalFraction& r) {
 	RationalFraction result;
 	result.den = this->den * r.den;
 	result.num = ((result.den / this->den) * this->num) - ((result.den / r.den) * r.num);
-	result.reduceFration();
+	result.reduceFraction();
 	return result;
 }
-
-RationalFraction RationalFraction::operator*(const RationalFraction& r) { // * operator overload
+RationalFraction RationalFraction::operator*(const RationalFraction& r) {
 	RationalFraction result;
 	result.den = this->den * r.den;
 	result.num = this->num * r.num;
-	result.reduceFration();
+	result.reduceFraction();
 	return result;
 }
-
-RationalFraction RationalFraction::operator/(const RationalFraction& r) { // / operator overload
+RationalFraction RationalFraction::operator/(const RationalFraction& r) {
 	RationalFraction result;
 	result.den = this->den * r.num;
 	result.num = this->num * r.den;
-	result.reduceFration();
+	result.reduceFraction();
 	return result;
 }
-
 bool RationalFraction::operator==(const RationalFraction& r) {
 	return (this->num * r.den == this->den * r.num);
 }
-
 bool RationalFraction::operator!=(const RationalFraction& r) {
 	return (this->num * r.den != this->den * r.num);
 }
-
 bool RationalFraction::operator>(const RationalFraction& r) {
-	return (this->num * r.den > this->den * r.num);
+	return (this->num * r.den > this->den* r.num);
 }
-
 bool RationalFraction::operator<(const RationalFraction& r) {
 	return (this->num * r.den < this->den * r.num);
 }
-
 bool RationalFraction::operator>=(const RationalFraction& r) {
 	return (this->num * r.den >= this->den * r.num);
 }
-
 bool RationalFraction::operator<=(const RationalFraction& r) {
 	return (this->num * r.den <= this->den * r.num);
 }
