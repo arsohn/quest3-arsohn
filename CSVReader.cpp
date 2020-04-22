3bbf0994a3e546cd79ede0b29be92c44726486d8
arsohn <60234960+arsohn@users.noreply.github.com>
2020-04-21 20:28:45 -0500

    Final Version
    should be final

--- CSVReader.cpp
+++ CSVReader.cpp
@@ -17,28 +17,28 @@ void CSVReader::compute() {
 		// result = result from operator overload
 		// HAVE TO RESEARCH HOW TO TURN CHAR TO INT, 
 		// atoi, strtok, strtok_s? need to do more research, check forums
-		r1.setNum
-		r2.setDen
+		r1.setNum(atoi(strtok_s(str, ",", &nextstr))); // get num of r1
+		r2.setDen(atoi(strtok_s(NULL, ",", &nextstr))); // get den of r1
 
-		sting operation = 
+		string operation = strtok_s(NULL, ",", &nextstr); // get operation
 
-		r2.setNum
-		r2.setDen
+		r2.setNum(atoi(strtok_s(NULL, ",", &nextstr)));    //get num of r2
+		r2.setDen(atoi(strtok_s(NULL, ",", &nextstr)));    //get den of r2
 
 		if (operation == "+") {
-			RationalFraction(r1 + r2);
+			RationalFraction result(r1 + r2);
 			cout << result.getNum() << "/" << result.getDen() << endl;
 		}
 		else if (operation == "-") {
-			RationalFraction(r1 - r2);
+			RationalFraction result(r1 - r2);
 			cout << result.getNum() << "/" << result.getDen() << endl;
 		}
 		else if (operation == "*") {
-			RationalFraction(r1 * r2);
+			RationalFraction result(r1 * r2);
 			cout << result.getNum() << "/" << result.getDen() << endl;
 		}
 		else if (operation == "/") {
-			RationalFraction(r1 / r2);
+			RationalFraction result(r1 / r2);
 			cout << result.getNum() << "/" << result.getDen() << endl;
 		}
 		else if (operation == "==") {
@@ -61,6 +61,7 @@ void CSVReader::compute() {
 		}
 	}
 }
+
 
 CSVReader::~CSVReader() {
 	ifs.close();
