976ecafcaa9fa8031921dacdea04a42d9e3f4e96
arsohn <60234960+arsohn@users.noreply.github.com>
2020-04-21 20:48:50 -0500

    FINAL CSVREADER.H

--- CSVReader.h
+++ CSVReader.h
@@ -1,20 +1,17 @@
-#pragma once
 #ifndef CSVREADER_H
 #define CSVREADER_H
 #include <iostream>
 #include <string>
 #include <fstream>
 #include "RationalFraction.h"
-
 using namespace std;
 
 class CSVReader {
 public:
 	CSVReader(string n);
-	void computer();
+	void compute();
 	~CSVReader();
 private:
 	fstream ifs;
 };
-
 #endif
